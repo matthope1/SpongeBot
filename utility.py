71ef3debee192b30b1f2af96c760661e99721456
matt! <matthope1@users.noreply.github.com>
2022-02-01 15:38:40 +0000

    added get_user_object function

--- utility.py
+++ utility.py
@@ -1,1 +1,16 @@
- 
+from replit import db
+
+def get_user_object(username):
+  # returns user object from database  
+  user = False
+  try: 
+    # for user in raw_admin_list:
+    for i in range(len(db["adminList"])):
+      if db["adminList"][i]["username"].lower() == username.lower():
+        user = db["adminList"][i].value
+        break
+    
+  except Exception as e:
+    print("Get user object error", e)
+
+  return user
